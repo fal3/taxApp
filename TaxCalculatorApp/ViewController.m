b813992e4aceb8da2c00e0135453ef821b41f9c3
fallaha13 <fallaha@wit.edu>
2015-05-06 20:59:09 -0400

    tax app commit

--- TaxCalculatorApp/ViewController.m
+++ TaxCalculatorApp/ViewController.m
@@ -10,18 +10,56 @@
 
 @interface ViewController ()
 
+
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+
+
+
+@property double caTax;
+@property double chiTax;
+@property double nyTax;
+
+
+
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    self.caTax = .075;
+    self.chiTax = .0925;
+    self.nyTax = .045;
+    
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+- (IBAction)onCalculateButtonTapped:(id)sender {
+
+    NSString *enteredText = self.priceTextField.text;
+    //storing a variable as NSString so we can convert to int
+
+    double enteredValue = enteredText.doubleValue;
+    //integer conversion
+    
+    if (self.segmentedControl.selectedSegmentIndex == 0) {
+        double result = self.caTax * enteredValue;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+    }
+    else if (self.segmentedControl.selectedSegmentIndex == 1){
+       double result = self.chiTax * enteredValue;
+       self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+
+    }else{
+        double result = self.nyTax * enteredValue;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+
+    }
+
+
 }
 
 @end
